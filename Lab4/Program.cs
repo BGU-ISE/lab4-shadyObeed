69ae89a7e2217e24b7afa47f2fa9cb944f077d6f
dimakagan15@gmail.com <Dima>
2019-03-14 21:44:43 +0200

    Conig log4net and add an example

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -3,19 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using log4net;
 
 namespace Lab4
 {
 
     class Program
     {
-        //private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-
+        //Declare an instance for log4net
+        private static readonly ILog Log =
+              LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         static void Main(string[] args)
         {
-            //log.Info("Hello logging world!");
-            Console.WriteLine("Hit enter");
-            Console.ReadLine();
+            ImplementLoggingFuntion();
+        }
+
+        private static void ImplementLoggingFuntion()
+        {
+            /* We have 5 levels of log message. Let's test all.
+             *  FATAL
+                ERROR
+                WARN
+                INFO
+                DEBUG
+             */
+            var secs = 3;
+            Log.Fatal("Start log FATAL...");
+            Console.WriteLine("Start log FATAL...");
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
+
+            Log.Error("Start log ERROR...");
+            Console.WriteLine("Start log ERROR...");
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
+
+            Log.Warn("Start log WARN...");
+            Console.WriteLine("Start log WARN...");
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
+
+            Log.Info("Start log INFO...");
+            Console.WriteLine("Start log INFO...");
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
+
+            Log.Debug("Start log DEBUG...");
+            Console.WriteLine("Start log DEBUG...");
+            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
+
+            Console.WriteLine("Press any key to close the application");
+            Console.ReadKey();
         }
     }
 }
