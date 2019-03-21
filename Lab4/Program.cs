8e67b7ae22be9b78cbd7c974e54698b2f61801bf
shady <obeed@post.bgu.ac.il>
2019-03-21 12:21:10 +0200

    first commit

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Threading;
 using log4net;
 
+
 namespace Lab4
 {
 
@@ -16,41 +17,104 @@ namespace Lab4
               LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         static void Main(string[] args)
         {
-            ImplementLoggingFuntion();
+            Login();
         }
-
-        private static void ImplementLoggingFuntion()
+        public Program()
         {
-            /* We have 5 levels of log message. Let's test all.
-             *  FATAL
-                ERROR
-                WARN
-                INFO
-                DEBUG
-             */
-            var secs = 3;
-            Log.Fatal("Start log FATAL...");
-            Console.WriteLine("Start log FATAL...");
-            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
 
-            Log.Error("Start log ERROR...");
-            Console.WriteLine("Start log ERROR...");
-            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
-
-            Log.Warn("Start log WARN...");
-            Console.WriteLine("Start log WARN...");
-            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
-
-            Log.Info("Start log INFO...");
-            Console.WriteLine("Start log INFO...");
-            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
-
-            Log.Debug("Start log DEBUG...");
-            Console.WriteLine("Start log DEBUG...");
-            Thread.Sleep(TimeSpan.FromSeconds(secs)); // Sleep some secs
-
-            Console.WriteLine("Press any key to close the application");
-            Console.ReadKey();
         }
+        private static void Login()
+        {
+            string username = "shady";
+            String password = "cTmrqm4h";
+            //int.Parse(Console.ReadLine());
+            Console.WriteLine("INTER USERNAME");
+
+            String input = Console.ReadLine();
+            if (username.Equals(input))
+            {
+                Console.WriteLine("correct username");
+                Console.ReadLine();
+            }
+            for (int i = 0; i < length; i++)
+            {
+                for (int i = length - 1; i >= 0; i--)
+                {
+
+                }
+
+            }
+            // override object.Equals
+    public override bool Equals(object obj)
+        {
+            //       
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237  
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            // TODO: write your implementation of Equals() here
+            throw new NotImplementedException();
+            return base.Equals(obj);
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            // TODO: write your implementation of GetHashCode() here
+            throw new NotImplementedException();
+            return base.GetHashCode();
+        }
+        public Program()
+        {
+            
+        }
+        public inst MyProperty { get; set; }
     }
+}
+public int MyProperty { get; set; }
+public ClassNamePlaceholder()
+{
+
+}
+private int myVar;
+Console.WriteLine("weh");0
+{
+    get { return myVar; }
+    set { myVar = value; }
+}
+    // override object.Equals
+    public override bool Equals(object obj)
+{
+    //       
+    // See the full list of guidelines at
+    //   http://go.microsoft.com/fwlink/?LinkID=85237  
+    // and also the guidance for operator== at
+    //   http://go.microsoft.com/fwlink/?LinkId=85238
+    //
+
+    if (obj == null || GetType() != obj.GetType())
+    {
+        return false;
+    }
+
+    // TODO: write your implementation of Equals() here
+    throw new NotImplementedException();
+    return base.Equals(obj);
+}
+ 
+// override object.GetHashCode
+public override int GetHashCode()
+{
+    // TODO: write your implementation of GetHashCode() here
+    throw new NotImplementedException();
+    return base.GetHashCode();
+}
+
 }
